a2c22ef89692fc480d00f476c530d37f536fac30
Mehul-N265 <mehulnimbalkar265@gmail.com>
2021-08-19 08:20:06 +0530

    Made another route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Maverick Leave System' });
-});
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Maverick | Login' });
+  res.render('index', { title: 'Maverick Leave System' });
 });
 module.exports = router;
